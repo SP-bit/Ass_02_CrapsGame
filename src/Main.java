bc2aa87f01928b218588790658a11b0871b51282
Sathwik <ps.bits5@gmail.com>
2024-10-20 19:57:51 -0400

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,81 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Scanner;
+import java.util.Random;
+public class Main
+{
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        boolean loopGame = true;
+        do
+        {
+            Random rnd = new Random();
+            Scanner in = new Scanner(System.in);
+            int die1 = rnd.nextInt(6) + 1;
+            int die2 = rnd.nextInt(6) + 1;
+            int crapsRoll = die1 + die2;
+            System.out.println("Die 1: " + die1);
+            System.out.println("Die 2: " + die2);
+            System.out.println("Sum (Die 1 + Die 2): " + crapsRoll + "\n");
+            loopGame = false;
+            boolean sumLoop = true;
+            if (crapsRoll == 7 || crapsRoll == 11)
+            {
+                System.out.println("You got a natural. You win!\n");
+                System.out.print("Play Again (Y/N): ");
+                String playAgain = in.next();
+                if (playAgain.equalsIgnoreCase("Y"))
+                {
+                    loopGame = true;
+                }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            }
+            else if(crapsRoll == 2 || crapsRoll == 3 || crapsRoll == 12)
+            {
+                System.out.println("You crapped out! You lost!\n");
+                System.out.print("Play Again (Y/N): ");
+                String playAgain = in.next();
+                if (playAgain.equalsIgnoreCase("Y"))
+                {
+                    loopGame = true;
+                }
+            }
+            else
+            {
+                int point = crapsRoll;
+                System.out.println("The sum is now the point: " + point + "\n");
+                do {
+                    die1 = rnd.nextInt(6) + 1;
+                    die2 = rnd.nextInt(6) + 1;
+                    int otherRoll = die1 + die2;
+                    System.out.println("Die 1: " + die1);
+                    System.out.println("Die 2: " + die2);
+                    System.out.println("Sum: " + otherRoll + "\n");
+                    if (otherRoll == 7) {
+                        sumLoop = false;
+                        System.out.println("You got a 7! You lost!\n");
+                        System.out.print("Play Again (Y/N): ");
+                        String playAgain = in.next();
+                        if (playAgain.equalsIgnoreCase("Y")) {
+                            loopGame = true;
+                        }
+
+                    } else if (otherRoll == crapsRoll)
+                    {
+                        sumLoop = false;
+                        System.out.println("You got the point sum! You win!\n");
+                        System.out.print("Play Again (Y/N): ");
+                        String playAgain = in.next();
+                        if (playAgain.equalsIgnoreCase("Y"))
+                        {
+                            loopGame = true;
+                        }
+                    }
+
+                } while(sumLoop);
+
+
+            }
         }
+        while (loopGame);
+        System.out.println("Thanks for playing");
+
     }
 }
